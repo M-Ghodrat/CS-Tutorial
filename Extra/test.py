b0fb59c36513adf385c62b563cb424b04115e71e
M-Ghodrat <ghodrat@ualberta.ca>
2021-03-22 17:49:03 -0600

    remove some Extra files

--- Extra/test.py
+++
@@ -1,9 +0,0 @@
-import my_panda as pd
-
-mydataset = {'cars': ['BMW', 'Volvo', 'Ford', 'Benz'], 'passings': [3, 7, 2, 5]}
-
-test = pd.DataFrame(mydataset)
-
-print('number of rows:', test.num_rows)
-print('number of columns:', test.num_columns)
-print('size:', test.size)
